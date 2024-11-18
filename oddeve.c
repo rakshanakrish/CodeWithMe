69b8fd7233b63e5b5008e48874c682014e40ec01
Subash Kumar <subashsk11831@gmail.com>
2024-11-18 13:27:10 +0000

    Nov 19, 2024, 12:27 AM

--- oddeve.c
+++ oddeve.c
@@ -5,7 +5,7 @@ int main()
     int num;
     printf("Enter a Number: ");
     scanf("%d", &num);
-    if (num % 2 == 0){
+    if (num == 0){
         printf("It is Even");
     } else if (num % 2 == 0) {
        printf("It is Even");
