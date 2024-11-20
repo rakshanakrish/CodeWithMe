c2fcd6df56b7662c7c32c003335a9b17157899ef
rakshanakrish <rakshanakrishnan42@gmail.com>
2024-11-20 20:46:06 +0530

    hello added

---
+++ Java/hello.java
@@ -0,0 +1,13 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class hello{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        int age = scan.nextInt();
+        System.out.println("My name is "+name);
+        System.out.println("My age is "+age);
+    }
+}
