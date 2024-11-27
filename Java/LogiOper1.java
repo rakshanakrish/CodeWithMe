112f9535231bae5b7c2bfbb9fc853195b67f261d
rakshanakrish <rakshanakrishnan42@gmail.com>
2024-11-27 22:48:58 +0530

    logical op prob added

---
+++ Java/LogiOper1.java
@@ -0,0 +1,19 @@
+public class LogiOper1 {
+    public static void main(String args[])
+    {
+        boolean hungry = true;
+        boolean icecream = true;
+
+
+        if(hungry || icecream) //(And &&) //(Or ||)
+        {
+            System.out.println("eat");
+        }
+        else
+        {
+            System.out.println("not eat");
+        }
+
+    }
+    
+}
