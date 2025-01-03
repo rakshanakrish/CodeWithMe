c6c34d8bbeafb05fdbe253c229e09d3c3d99891d
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-03 23:03:22 +0530

    add2num added

---
+++ Java/add2num.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class add2num {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter a value: ");
+        int a = scan.nextInt();
+        System.out.print("enter b value: ");
+        int b = scan.nextInt();
+        int sum = a + b;
+
+        System.out.println("sum: "+sum);
+
+    
+}
+}
