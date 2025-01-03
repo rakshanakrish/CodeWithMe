e60de645a9ff1eaa38fca158e46d2978b040dab6
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-03 23:01:43 +0530

    array added

---
+++ Java/array.java
@@ -0,0 +1,8 @@
+public class array {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        System.out.println("Array element: ");
+        for (int i = 0, i< arr.lenght, i++);
+        
+    
+}}
