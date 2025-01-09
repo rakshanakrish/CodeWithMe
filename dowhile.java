3f16e3c8b6966a6d405f02bf30fcd91020e44fd8
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-09 13:52:51 +0530

    files added

---
+++ dowhile.java
@@ -0,0 +1,14 @@
+public class dowhile {
+    public static void main(String[]args){
+        int i =1;
+        int sum = 0;
+        int n = 10;
+        do{
+            sum = sum+i;
+            i = i+2;
+            System.out.println(i);
+        }while(i<=n);
+    } 
+    
+}
+  
