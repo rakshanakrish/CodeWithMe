a4b557a30c78cbad0b6d32d2814814844cf76d1b
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-21 19:01:33 +0530

    lil adv folder created

---
+++ Java/lil adv/classOb.java
@@ -0,0 +1,20 @@
+class students
+{
+    int Age;
+    int Reg_no;
+    String Name;
+}
+class StudentDetails
+{
+    public static void main(String[] args) {
+        students s1 = new students();
+        s1.Age = 20;
+        s1.Reg_no = 7128;
+        s1.Name = "Rakshana";
+     System.out.println(s1.Name);
+     students s2 = new students();
+     s2.Age = 21;
+     s2.Reg_no = 7129;
+     System.out.println(s2.Age);
+    }
+}
