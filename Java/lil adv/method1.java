1c8527794e217bbc01d9dbf0377d7049e024f184
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-21 19:02:26 +0530

    method added

---
+++ Java/lil adv/method1.java
@@ -0,0 +1,10 @@
+public class method1 {
+    static void printline(){
+        System.out.println("Static method called");
+    }
+    public static void main(String[] args) {
+        //method1.printline();
+        printline();
+    }
+    
+}
