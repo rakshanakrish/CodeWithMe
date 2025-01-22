ca14a82148ba19eb7b14c5bb2b4293566625111b
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-22 18:11:36 +0530

    methods added

--- Java/lil_adv/methods.java
+++ Java/lil_adv/methods.java
@@ -1,5 +1,14 @@
 package lil_adv;
 
 public class methods {
+    public static void main(String[] args) {
+        add();
+    }
+    public static void add() { //void doesnt return anything
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+    }
     
 }
