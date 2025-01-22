ed71cc56542eec7e75b9f7b844d2844ed4970722
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-22 18:13:23 +0530

    added

--- Java/lil_adv/methods.java
+++ Java/lil_adv/methods.java
@@ -3,6 +3,7 @@ package lil_adv;
 public class methods {
     public static void main(String[] args) {
         add();
+        System.out.println("c is printed");
     }
     public static void add() { //void doesnt return anything
         int a = 10;
