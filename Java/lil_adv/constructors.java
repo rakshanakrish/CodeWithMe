bcb91583dc8c6c3c2718835895bb4bc0e534cb2b
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-23 22:43:20 +0530

    added constructors

---
+++ Java/lil_adv/constructors.java
@@ -0,0 +1,15 @@
+package lil_adv;
+class sample{
+    int rollno;
+    int marks;
+
+}
+
+class constructors {
+    public static void main(String[] args) {
+        sample obj = new sample();
+        System.out.println(obj.marks);
+        System.out.println(obj.rollno);
+    }
+    
+}
