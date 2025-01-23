400efa0cd6c6367603873e6cea53203dababb59a
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-23 18:33:03 +0530

    new upd added

---
+++ Java/lil_adv/classObj.java
@@ -0,0 +1,10 @@
+package lil_adv;
+public class classObj {
+    String name;
+    int rollno;
+    public static void main(String[] args) {
+        classObj att = new classObj(); //new for memory allocation
+    }
+
+    
+}
