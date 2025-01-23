12c0d742f27bfd3c16b2a40d4f865db06c317545
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-23 18:40:57 +0530

    updated

--- Java/lil_adv/classObj.java
+++ Java/lil_adv/classObj.java
@@ -2,9 +2,17 @@ package lil_adv;
 public class classObj {
     String name;
     int rollno;
-    public static void main(String[] args) {
-        classObj att = new classObj(); //new for memory allocation
-    }
+   
+    public void display() {
+        System.out.println(name);
+        System.out.println(rollno);}
+
+public static void main(String[] args) {
+    classObj att = new classObj(); //new for memory allocation
+    att.name = "Rakshana";
+    att.rollno = 67;
+    att.display();
+        }
 
     
 }
