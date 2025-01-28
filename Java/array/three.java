dc551708709322e6b70dd65a4f6caa87de8f36dc
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-28 20:59:09 +0530

    print the square root of each element

--- Java/array/three.java
+++ Java/array/three.java
@@ -1,14 +1,14 @@
-// //print the square root of each element
-// package array;
-// import java.util.*;
-// public class three {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         int[] numbers = new int[100]; 
-//         System.out.println("Square roots of the array elements:");
-//         for (int i = 0; i < numbers.length; i++) {
-//             numbers[i] = sc.nextInt(); 
-//             System.out.printf("Square root of %d is %.3f\n", numbers[i], Math.sqrt(numbers[i]));
-//         }
-//     }
-// }
+//print the square root of each element
+package array;
+import java.util.*;
+public class three {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[100]; 
+        System.out.println("Square roots of the array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt(); 
+            System.out.printf("Square root of %d is %.3f\n", numbers[i], Math.sqrt(numbers[i]));
+        }
+    }
+}
