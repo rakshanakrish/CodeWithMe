d8d26a695d0f18257714ad140a6c0c4da268432c
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-28 20:58:30 +0530

    new sum added

--- Java/array/two.java
+++ Java/array/two.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class two {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int []arr = new int[100];
+        int n = sc.nextInt();
+        int []arr = new int[n];
         for(int i =0;i<arr.length;i++){
             arr[i]= sc.nextInt();
             if(arr[i]>9 && arr[i]<100){
