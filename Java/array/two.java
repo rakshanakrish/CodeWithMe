fbba47eece14b13fa5970d817175337f3107e36e
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-28 20:30:14 +0530

    print the two digit numbers

--- Java/array/two.java
+++ Java/array/two.java
@@ -1,6 +1,17 @@
+//print the two digit numbers
 package array;
-
+import java.util.*;
 public class two {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int []arr = new int[100];
+        for(int i =0;i<arr.length;i++){
+            arr[i]= sc.nextInt();
+            if(arr[i]>9 && arr[i]<100){
+                System.out.println(arr[i]);
+            }
+        }
+        
+    }
     
 }
