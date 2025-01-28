e3e4965ca71d0173547042ddb3eab6f32cb42916
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-28 20:16:44 +0530

    printing even numbers and then odd numbers

--- Java/array/one.java
+++ Java/array/one.java
@@ -1,15 +1,30 @@
 package array;
 import java.util.*;
-
+//printing of even numbers and then odd numbers
 public class one {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] arr = new int[6];
-        for(int i= 0;i<arr.length;i++){
+        //Scanner sc = new Scanner(System.in);
+        //int[] arr = new int[10];
+        int[]arr = {1,2,3,4,5,6,7,8};
+        System.out.println("Even numbers are: ");
+        for(int i =0;i<arr.length;i++){
+            //arr[i] = sc.nextInt();
+            if(arr[i]%2==0){
+                System.out.println(arr[i]);
+            }
+        
             
-            // arr[i] = sc.nextInt();
-            // System.out.println(arr[i]);
         }
-    }
+        System.out.println("Odd numbers are: ");
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]%2!=0){
+                System.out.println(arr[i]);
+            }
+        
+            
+        }
+        
+        
+    }}
     
-}
+
