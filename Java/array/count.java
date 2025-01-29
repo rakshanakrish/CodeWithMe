df9372f8193061ab4b77127b152d09c3867377f5
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-01-29 19:07:43 +0530

    count numbers ending with 5

--- Java/array/count.java
+++ Java/array/count.java
@@ -1,15 +1,15 @@
-// //count numbers ending with 5
-// package array;
+//count numbers ending with 5
+package array;
 
-// public class count {
-//     public static void main(String[] args) {
-//         int[] arr = {15, 20, 35, 42, 5};
-//         int count= 0;
-//         for(int i=0;i<arr.length;i++){
-//             if(arr[i]%10==5){
-//                 count++;
-//                 //System.out.println(arr[i]);
-//             }}
-//             System.out.println(count);
-//     }
-// }
+public class count {
+    public static void main(String[] args) {
+        int[] arr = {15, 20, 35, 42, 5};
+        int count= 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%10==5){
+                count++;
+                //System.out.println(arr[i]);
+            }}
+            System.out.println(count);
+    }
+}
