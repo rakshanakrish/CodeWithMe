a872bf6103b312919823344f1dab21d3284295e4
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-06-20 12:19:21 +0530

    ThirdLargestElement

---
+++ python/ThirdLargEle.py
@@ -0,0 +1,11 @@
+## Find 3rd largest element
+a=[4,5,8,7,6]
+for i in range(len(a)):
+    for j in range(i+1,len(a)):
+        if a[i]>a[j]:
+            a[i],a[j]=a[j],a[i]
+print(a[-3])
+
+#a=[4,5,8,7,6]
+#a.sort()
+#print(a[-3])
