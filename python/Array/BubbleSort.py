85765ab30d1c9fdf6ba0a52c7b160b14d23cfc7d
rakshanakrish <rakshanakrishnan42@gmail.com>
2025-07-26 15:14:13 +0530

    bubble sort

--- python/Array/BubbleSort.py
+++ python/Array/BubbleSort.py
@@ -1,7 +1,12 @@
 a=[1,3,4,7,6,0]
 n=len(a)
 for i in range(n-1):
+    swapped=False
     for j in range(n-i-1):
         if a[j]>a[j+1]:
             a[j],a[j+1]=a[j+1],a[j]
+            swapped=True
+    #By using swapped we can avoid unnecessay loop
+    if not swapped:
+        break
 print(a)
